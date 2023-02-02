385c817548ecd5ecb34fbac8826ff39b980d7771
Giovanni Fernandes Vicentin <giovannifvicentin@gmail.com>
2023-02-02 17:30:37 -0300

    add dae and av

---
+++ dt_documents/av.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+
+def move_files_av(source_dir, destinations):
+    for filename in os.listdir(source_dir):
+        for keyword, destination_dir in destinations:
+            if keyword in filename.split("-")[1]:
+                shutil.move(os.path.join(source_dir, filename), os.path.join(destination_dir, filename))
+                break
+
+source_dir = "C:\\DOCUMENTOS ROTINA"
+destinations = [("AV", "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\AvisosdeVencimento")]
+
+move_files_av(source_dir, destinations)
