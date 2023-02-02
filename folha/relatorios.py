ec440c9a1d233c6e295bb43edb9b3960401c59ec
Giovanni Fernandes Vicentin <giovannifvicentin@gmail.com>
2023-02-02 14:53:43 -0300

    add functions

--- folha/relatorios.py
+++ folha/relatorios.py
@@ -50,8 +50,9 @@ for filename in os.listdir(source_dir):
         shutil.move(os.path.join(source_dir, filename), os.path.join(destination_dir, filename))
 
 source_dir = "C:\\DOCUMENTOS ROTINA"
-destination_dir = "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\IRRF"
+destinations = [("ProgramaçãodeFérias", "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\Ferias"), 
+                ("ExtratoMensal", "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\Extratos"),
+                ("RecibodePagamento", "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\Holerites"),
+                ("RelatóriodeLíquidos", "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\Liquidos")]
 
-for filename in os.listdir(source_dir):
-    if "IRRF" in filename.split("-")[1]:
-        shutil.move(os.path.join(source_dir, filename), os.path.join(destination_dir, filename))
+move_files_folha(source_dir, destinations)
--- folha/test.py
+++ folha/test.py
@@ -1,29 +1,14 @@
-import os
-import shutil
+def move_files_folha(source_dir, destinations):
+    for filename in os.listdir(source_dir):
+        for keyword, destination_dir in destinations:
+            if keyword in filename.split("-")[1]:
+                shutil.move(os.path.join(source_dir, filename), os.path.join(destination_dir, filename))
+                break
 
 source_dir = "C:\\DOCUMENTOS ROTINA"
-destination_dir = "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\Ferias"
+destinations = [("ProgramaçãodeFérias", "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\Ferias"), 
+                ("ExtratoMensal", "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\Extratos"),
+                ("RecibodePagamento", "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\Holerites"),
+                ("RelatóriodeLíquidos", "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\Liquidos")]
 
-for filename in os.listdir(source_dir):
-    if "ProgramaçãodeFérias" in filename.split("-")[1]:
-        shutil.move(os.path.join(source_dir, filename), os.path.join(destination_dir, filename))
-source_dir = "C:\\DOCUMENTOS ROTINA"
-destination_dir = "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\Extratos"
-
-for filename in os.listdir(source_dir):
-    if "ExtratoMensal" in filename.split("-")[1]:
-        shutil.move(os.path.join(source_dir, filename), os.path.join(destination_dir, filename))
-
-        source_dir = "C:\\DOCUMENTOS ROTINA"
-destination_dir = "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\Holerites"
-
-for filename in os.listdir(source_dir):
-    if "RecibodePagamento" in filename.split("-")[1]:
-        shutil.move(os.path.join(source_dir, filename), os.path.join(destination_dir, filename))
-
-source_dir = "C:\\DOCUMENTOS ROTINA"
-destination_dir = "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\Liquidos"
-
-for filename in os.listdir(source_dir):
-    if "RelatóriodeLíquidos" in filename.split("-")[1]:
-        shutil.move(os.path.join(source_dir, filename), os.path.join(destination_dir, filename))
+move_files_folha(source_dir, destinations)
