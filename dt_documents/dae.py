817b92a5a9fce2f9107f2240d75d92a34bd5fab7
Giovanni Fernandes Vicentin <giovannifvicentin@gmail.com>
2023-02-08 11:52:45 -0300

    testing a new code in test with OOP

--- dt_documents/dae.py
+++ dt_documents/dae.py
@@ -1,14 +1,41 @@
-import os
-import shutil
+class Move_files_done:
+    destinations = {
+        'ProgramaçãodeFérias': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\Ferias',
+        'ExtratoMensal': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\Extratos',
+        'RecibodePagamento': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\Holerites',
+        'RelatóriodeLíquidos': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\Liquidos',
+        'AV': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\AvisosdeVencimento',
+        'DAE': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\DAE',
+        'IRRF': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\IRRF',
+        'GRF': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\FGTS',
+        'Guia': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\INSS',
+    }
 
-def move_files_dae(source_dir, destinations):
-    for filename in os.listdir(source_dir):
-        for keyword, destination_dir in destinations:
-            if keyword in filename.split("-")[1]:
-                shutil.move(os.path.join(source_dir, filename), os.path.join(destination_dir, filename))
-                break
+    destinations_adto = {
+        'ExtratoMensal': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\ExtratosA',
+        'RecibodePagamento': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\HoleritesA',
+        'RelatóriodeLíquidos': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\LiquidosA',
+        'AV': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\AvisosdeVencimento',
+        'DAE': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\DAE',
+        'IRRF': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\IRRF',
+        'GRF': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\FGTS',
+        'Guia': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\INSS',
+    }
 
-source_dir = "C:\\DOCUMENTOS ROTINA"
-destinations = [("DAE", "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\DAE")]
+    def __init__(self, source_dir, adto=False):
+        self.source_dir = 'C:\\DOCUMENTOS ROTINA'
+        self.destinations = self.destinations if not adto else self.destinations_adto
 
-move_files_dae(source_dir, destinations)
+    def moving(self):
+        for filename in os.listdir(self.source_dir):
+            keyword = filename.split("-")
+
+            if keyword[2] in ["Guia", "GRF"]:
+                keyword = keyword[2]
+            else:
+                keyword = keyword[1]
+
+            if keyword in self.destinations:
+                shutil.move(os.path.join(self.source_dir, filename), os.path.join(self.destinations[keyword], filename))
+
+
--- dt_documents/test.py
+++ dt_documents/test.py
@@ -1,32 +1,87 @@
 import os
 import shutil
 
-# Function to move files from one folder to another based on keyword
-def move_files(source_dir, destinations):
-    for filename in os.listdir(source_dir):
-        for keyword, destination_dir in destinations:
-            # Check if keyword is in the filename
-            if keyword in filename.split("-")[1]:
-                # Move the file to the destination folder
-                shutil.move(os.path.join(source_dir, filename), os.path.join(destination_dir, filename))
-                break
 
-# Move files from Extratos folder
-source_dir = "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\Extratos"
-destinations = [("ExtratoMensal", "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\Extratos")]
-move_files(source_dir, destinations)
+class Move_files:
 
-# Move files from Holerites folder
-source_dir = "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\DominioWebDocumentos\\3@ DOING\\Holerites"
-destinations = [("RecibodePagamento", "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\Holerites")]
-move_files(source_dir, destinations)
+    destinations = {
+        'ProgramaçãodeFérias': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\Ferias',
+        'ExtratoMensal': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\Extratos',
+        'RecibodePagamento': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\Holerites',
+        'RelatóriodeLíquidos': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\Liquidos',
+        'AV': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\AvisosdeVencimento',
+        'DAE': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\DAE',
+        'IRRF': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\IRRF',
+        'GRF': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\FGTS',
+        'Guia': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\INSS',
+    }
 
-# Move files from Liquidos folder
-source_dir = "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\DominioWebDocumentos\\3@ DOING\\Liquidos"
-destinations = [("RelatóriodeLíquidos", "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\Liquidos")]
-move_files(source_dir, destinations)
+    destinations_adto = {
+        'ExtratoMensal': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\ExtratosA',
+        'RecibodePagamento': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\HoleritesA',
+        'RelatóriodeLíquidos': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\LiquidosA',
+        'AV': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\AvisosdeVencimento',
+        'DAE': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\DAE',
+        'IRRF': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\IRRF',
+        'GRF': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\FGTS',
+        'Guia': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\INSS',
+    }
 
-# Move files from Ferias folder
-source_dir = "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\DominioWebDocumentos\\3@ DOING\\Ferias"
-destinations = [("ProgramaçãodeFérias", "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\Ferias")]
-move_files(source_dir, destinations)
+    def __init__(self, source_dir, adto=False):
+        self.source_dir = 'C:\\DOCUMENTOS ROTINA'
+        self.destinations = self.destinations if not adto else self.destinations_adto
+
+    def moving(self):
+        for filename in os.listdir(self.source_dir):
+            keyword = filename.split("-")
+
+            if keyword[2] in ["Guia", "GRF"]:
+                keyword = keyword[2]
+            else:
+                keyword = keyword[1]
+
+            if keyword in self.destinations:
+                shutil.move(os.path.join(self.source_dir, filename), os.path.join(self.destinations[keyword], filename))
+
+
+class Move_files_done:
+    destinations = {
+        'ProgramaçãodeFérias': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\Ferias',
+        'ExtratoMensal': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\Extratos',
+        'RecibodePagamento': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\Holerites',
+        'RelatóriodeLíquidos': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\Liquidos',
+        'AV': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\AvisosdeVencimento',
+        'DAE': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\DAE',
+        'IRRF': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\IRRF',
+        'GRF': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\FGTS',
+        'Guia': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\INSS',
+    }
+
+    destinations_adto = {
+        'ExtratoMensal': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\ExtratosA',
+        'RecibodePagamento': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\HoleritesA',
+        'RelatóriodeLíquidos': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\LiquidosA',
+        'AV': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\AvisosdeVencimento',
+        'DAE': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\DAE',
+        'IRRF': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\IRRF',
+        'GRF': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\FGTS',
+        'Guia': 'C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\INSS',
+    }
+
+    def __init__(self, source_dir, adto=False):
+        self.source_dir = 'C:\\DOCUMENTOS ROTINA'
+        self.destinations = self.destinations if not adto else self.destinations_adto
+
+    def moving(self):
+        for filename in os.listdir(self.source_dir):
+            keyword = filename.split("-")
+
+            if keyword[2] in ["Guia", "GRF"]:
+                keyword = keyword[2]
+            else:
+                keyword = keyword[1]
+
+            if keyword in self.destinations:
+                shutil.move(os.path.join(self.source_dir, filename), os.path.join(self.destinations[keyword], filename))
+
+
