e1c2a7b9264401658b60e3a21be7e5cc1c719c70
Giovanni Fernandes Vicentin <giovannifvicentin@gmail.com>
2023-02-15 13:33:55 -0300

    adto_done working

---
+++ dt_documents/adto_done.py
@@ -0,0 +1,27 @@
+import os
+import shutil
+
+# Function to move files from one folder to another based on keyword
+def move_files(source_dir, destinations):
+    for filename in os.listdir(source_dir):
+        for keyword, destination_dir in destinations:
+            # Check if keyword is in the filename
+            if keyword in filename.split("-")[1]:
+                # Move the file to the destination folder
+                shutil.move(os.path.join(source_dir, filename), os.path.join(destination_dir, filename))
+                break
+            
+# Move files from Extratos folder
+source_dir = "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\3@ DOING\\ExtratosA"
+destinations = [("ExtratoMensal", "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\ExtratosA")]
+move_files(source_dir, destinations)
+
+# Move files from Holerites folder
+source_dir = "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\DominioWebDocumentos\\3@ DOING\\HoleritesA"
+destinations = [("RecibodePagamento", "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\HoleritesA")]
+move_files(source_dir, destinations)
+
+# Move files from Liquidos folder
+source_dir = "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\DominioWebDocumentos\\3@ DOING\\LiquidosA"
+destinations = [("RelatóriodeLíquidos", "C:\\Users\\TALST-GiovanniVicent\\TALST CONTABILIDADE\\TALST CONTABILIDADE - 5.7.2 AUTOMACAO\\DominioWebDocumentos\\4@ DONE\\LiquidosA")]
+move_files(source_dir, destinations)
