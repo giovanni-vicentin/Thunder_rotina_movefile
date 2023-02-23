eb274b8b76a7b44d58accc4d0d3b4d7c8bb1dae6
Giovanni Fernandes Vicentin <giovannifvicentin@gmail.com>
2023-02-23 14:48:56 -0300

    Created a questionnaire and removed unused files.

--- question/questioner.py
+++ question/questioner.py
@@ -1,28 +1,95 @@
-from dt_documents.test import Move_files
-from dt_documents.test import Move_files_done
 
-
-def ask_yes_no(input_text):
+def questionnaire():
     while True:
-        response = input(f'{input_text}Type your answer (yes/no): ').lower()
-        if response == "yes" or response == "y" or response == "sim" or response == "s":
-            return False
-        elif response == "no" or response == "n" or response == "nao" or response == "não":
-            return True
-        elif response == "cancelar" or response == "cancel":
-            exit()
+        print('Welcome to Thunder_rotina_movefile\n')
+        print('Choose an option:\n')
+        print('1. Move payroll files to "Doing" folder\n')
+        print('2. Move payroll files to "Done" folder\n')
+        print('3. Move taxes "FGTS" for example\n')
+        print('4. To exit the automation\n')
+
+        option = input('Enter your option (1, 2, 3 or 4): ')
+
+        if option == '1':
+            while True:
+                print('You chose move payroll files to "Doing" folder.\n')
+                print('Choose a sub-option:\n')
+                print('1. For prepayments payroll documents\n')
+                print('2. For regular payroll documents\n')
+                print('3. To exit the automation\n')
+
+                sub_option = input('Enter your sub-option (1, 2 or 3): ')
+
+                if sub_option == '1':
+                    print('You chose move prepayments payroll documents to "Doing" folder.\n')
+
+                    break
+                elif sub_option == '2':
+                    print('You chose move regular payroll documents to "Doing" folder.\n')
+                    break
+                elif sub_option == '3':
+                    print('You chose to exit the automation.\n')
+                    break
+                else:
+                    print('Invalid sub-option. Please enter a valid sub-option.\n')
+            break
+        elif option == '2':
+            while True:
+                print('You chose move payroll files to "Done" folder.\n')
+                print('Choose a sub-option:\n')
+                print('1. For prepayments payroll documents\n')
+                print('2. For regular payroll documents\n')
+                print('3. To exit the automation\n')
+
+                sub_option = input('Enter your sub-option (1, 2 or 3): ')
+
+                if sub_option == '1':
+                    print('You chose move prepayments payroll documents to "Done" folder.\n')
+                    break
+                elif sub_option == '2':
+                    print('You chose move regular payroll documents to "Done" folder.\n')
+                    break
+                elif sub_option == '3':
+                    print('You chose to exit the automation.\n')
+                    break
+                else:
+                    print('Invalid sub-option. Please enter a valid sub-option.\n')
+            break
+        elif option == '3':
+            while True:
+                print('You chose move taxes documents.\n')
+                print('Choose a sub-option:\n')
+                print('1. Pull FGTS docs from "RPAGFIP" to "Documentos Rotina".\n')
+                print('2. Pull INSS docs from "RPADCTFWEB" to "Documentos Rotina".\n')
+                print('3. Move taxes files to "Doing" folder\n')
+                print('4. Move taxes files to "Done" folder\n')
+                print('5. To exit the automation\n')
+
+                sub_option = input('Enter your sub-option (1, 2, 3, 4 or 5): ')
+
+                if sub_option == '1':
+                    print('You chose move FGTS documents to "Documentos Rotina" folder.\n')
+                    break
+                elif sub_option == '2':
+                    print('You chose move INSS documents to "Documentos Rotina" folder.\n')
+                    break
+                elif sub_option == '3':
+                    print('You chose move taxes documents to "Doing" folder.\n')
+                    break
+                elif sub_option == '4':
+                    print('You chose move taxes documents to "Done" folder.\n')
+                    break
+                elif sub_option == '5':
+                    print('You chose to exit the automation.\n')
+                    break
+                else:
+                    print('Invalid sub-option. Please enter a valid sub-option.\n')
+            break
+        elif option == '4':
+            print('You chose to exit the automation.\n')
+            break
         else:
-            print('Invalid response, please type again.\n\nTo cancel, type "cancel".\n')
+            print('Invalid option. Please enter a valid option.\n')
 
 
-def ask_move_files():
-    doing_done = ask_yes_no('Do these documents not go in the "DONE" folder?\n')
-    is_adto = ask_yes_no('Are these documents not "adiantamento" documents?\n')
-
-    if doing_done:
-        Move_files(is_adto)
-    else:
-        Move_files_done(is_adto)
-
-
-ask_move_files()
+questionnaire()
